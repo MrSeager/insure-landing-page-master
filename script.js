e87dff889e1fc13d5c04c029c36cac9794ba302d
MrSeager <seager1993@gmail.com>
2024-01-29 12:07:31 +0200

    1/29/2024

---
+++ script.js
@@ -0,0 +1,7 @@
+let openNavMenu = () => {
+	if (document.getElementById("navMob").style.display != "flex"){
+		document.getElementById("navMob").style.display = "flex";
+	} else{
+		document.getElementById("navMob").style.display = "none";
+	}
+}
